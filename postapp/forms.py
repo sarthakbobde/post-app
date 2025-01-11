a4114f5dcadca9380ef81cffb04c0e8fd84610ba
Sarthak <bobdesarthak@gmail.com>
2025-01-11 10:49:40 +0530

    User Model

--- postapp/forms.py
+++ postapp/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
+        # it is list because we created form
         fields = ['text', 'photo']
+        
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        # it is a tuple because it is built in form
+        fields = ()
--- postapp/views.py
+++ postapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -11,6 +12,7 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('created_at')
     return render(request, 'tweet_list.html', {'tweets':tweets})
 
+@login_required
 def tweet_create(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
@@ -23,6 +25,7 @@ def tweet_create(request):
         form = TweetForm()
     return render(request, 'tweet_form.html', {'form':form})
 
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method == 'POST':
@@ -36,6 +39,7 @@ def tweet_edit(request, tweet_id):
         form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form':form})
 
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method == 'POST':
